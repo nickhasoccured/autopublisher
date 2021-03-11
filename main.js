d16ee3b38f30bd027d5f9c15dcbe700ca7e2f936
Nicholas Christopher <nchristopher@tuta.io>
2021-03-11 11:59:04 -0800

    Optimized a few things (v2.1.0)
    - Used a global infoEmbed variable. Since this never changes, there's no point in redefining it each time.
    - Removed console.error() when failing to crosspost message/message owner. These events would only occur due to user error, which is out of my control.

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
+console.log(`Starting AutoPublisher v${require("./package.json").version}...`);
+
 const Discord = require("discord.js");
 
 const config = require("./config.json");
 
 const client = new Discord.Client();
+
+const infoEmbed = new Discord.MessageEmbed()
+	.setColor("#2ea1de")
+	.setTitle("Information")
+	.setDescription(
+		`Autopublisher is a simple Discord bot that automatically publishes messages in announcement channels.
+
+[Source Code](https://github.com/nickhasoccured/autopublisher)`
+	)
+	.setThumbnail("https://i.imgur.com/ZMpfsAf.png")
+	.addFields({
+		name: "Common Issues",
+		value: `If I'm not automatically publishing messages, make sure that:
+	**1)** I have permissions to manage & read messages in that channel.
+	**2)** The channel is an announcement channel.
+	If you're sure there's a problem, open an issue on this bot's [GitHub](https://github.com/nickhasoccured/autopublisher)`,
+		inline: true,
+	});
 
 client.once("ready", () => {
 	console.log(`Logged in as ${client.user.tag} (${client.user.id})`);
@@ -25,16 +45,7 @@ client.once("ready", () => {
 
 client.on("message", (message) => {
 	if (message.channel.type === "news") {
-		message.crosspost().catch((error) => {
-			console.error(`Failed to crosspost message (${
-				message.id
-			}) in channel #${message.channel.name} (${message.channel.id}) ${
-				message.guild
-					? `of server ${message.guild.name} (${message.guild.id})`
-					: ""
-			}
-			${error}`);
-
+		message.crosspost().catch(() => {
 			const owner = client.users.resolve(message.guild.ownerID);
 			if (owner) {
 				const errorEmbed = new Discord.MessageEmbed()
@@ -43,42 +54,13 @@ client.on("message", (message) => {
 					.setDescription(
 						`There was an issue publishing a message in <#${message.channel.id}>, make sure I have permissions to do so!`
 					);
-				owner.send(errorEmbed).catch((error) => {
-					console.error(`Failed to send message to ${owner.tag} (${owner.id})
-						${error}`);
-				});
+				owner.send(errorEmbed).catch(() => {});
 			}
 		});
 	}
 
 	if (message.channel.type === "dm" && !message.author.bot) {
-		const embed = new Discord.MessageEmbed()
-			.setColor("#2ea1de")
-			.setTitle("Information")
-			.setDescription(
-				`Autopublisher is a simple Discord bot that automatically publishes messages in announcement channels.
-			
-			[Source Code](https://github.com/nickhasoccured/autopublisher)`
-			)
-			.setThumbnail("https://i.imgur.com/ZMpfsAf.png")
-			.addFields({
-				name: "Common Issues",
-				value: `If I'm not automatically publishing messages, make sure that
-				**1)** I have permissions to manage & read messages in that channel
-				**2)** The channel is an announcement channel
-				If you're sure there's a problem, open an issue on this bot's [GitHub](https://github.com/nickhasoccured/autopublisher)`,
-				inline: true,
-			});
-		message.channel.send(embed).catch((error) => {
-			console.error(`Failed to send message in channel #${
-				message.channel.name
-			} (${message.channel.id}) ${
-				message.guild
-					? `of server ${message.guild.name} (${message.guild.id})`
-					: ""
-			}
-			${error}`);
-		});
+		message.channel.send(infoEmbed).catch(() => {});
 	}
 });
 
